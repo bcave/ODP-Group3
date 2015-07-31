977180cb06051be0d42ecd76bde6ca3ed0049c05
bcave <benjamin.cave@theodi.org>
2015-07-31 15:08:44 +0100

    Colour change

--- map/js/config.js
+++ map/js/config.js
@@ -1,10 +1,10 @@
 var config = {};
 
-config.data_file = "data/mapdata.csv";
-config.column_title = "Population";
+config.data_file = "data/somefile.csv";
+config.column_title = "Some_Column";
 
 config.map_height = "400px";
 config.map_width = "800px";
 
-config.color_scale = "blue";
+config.color_scale = "red";
 config.color_multiplier = 5;
